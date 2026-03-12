9693b4e0a29112f4765b7ead032e464715cfc801
IlhanTaskin <ilhantaskin@std.iyte.edu.tr>
2026-03-12 20:55:29 +0300

    divide method added

--- custom_classes.py
+++ custom_classes.py
@@ -1,3 +1,9 @@
 class Calculator:
     def __init__(self):
         self._current_val = 0
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+        self._current_val = x / y
+        return self._current_val
