401628eb081e59bdb337bc60e908e1009105bd7c
IlhanTaskin <ilhantaskin@std.iyte.edu.tr>
2026-03-12 21:18:13 +0300

    final version

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from custom_classes import Calculator
 
 calculator = Calculator()
-print(calculator._current_val)
+result1=calculator.add(10,5)
+result2=calculator.multiply(result1,2)
+result3=calculator.divide(result2,3)
+print(result3)
